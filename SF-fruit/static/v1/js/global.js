f75f5186cc56a5dd14c7ff33407185f7c75431fe
quiet201 <278882232@qq.com>
2016-05-08 23:57:50 +0800

    0508

--- SF-fruit/static/v1/js/global.js
+++ SF-fruit/static/v1/js/global.js
@@ -94,6 +94,10 @@ seajs.use(['hammer','layer','public'], function(myHam,myLay,myPub) {
 
         });
 
+
+
+
+
         function addFixed () {
             var _scrollTop = $(window).scrollTop();
             var _time = null;
@@ -480,6 +484,8 @@ seajs.use(['hammer','layer','public'], function(myHam,myLay,myPub) {
                             clearTimeout(clearTime);
                             clearTime = setTimeout(function() {
                                 oShopCarTip.removeClass('mybounceIn');
+
+
                                 dbTapOff = true;
                             },800);
                         });
@@ -584,6 +590,63 @@ seajs.use(['hammer','layer','public'], function(myHam,myLay,myPub) {
         if(oUserMessage.length>0) {
             myPub.statInputNum(oUserMessage,maxLWord);
         }
+
+
+        // 搜索设置cookie
+        var cTime = 'h2';  //coookie 时间
+        var _val = [];
+        var oSearchBtn = $('.js_searchBtn');    //搜索按钮
+        var oSearchTxt = $('.js_searchTxt');    //搜索框
+        var oHClearBtn = $('.js_HClearBtn');  //清楚历史记录按钮
+        var oHisListUl = $('.js_hisList').find('ul'); //历史记录UL
+        var recomHTMl = $('.js_hisList').find('ul').html();  //系统推荐
+        var cookArry = document.cookie.split('; ');   // 分离cookie成数组
+        var cookIndex = parseInt(myPub.getCookie('cookIndex')); // 记录个数
+
+        // 获取cookie并设置历史记录
+        if(myPub.getCookie('cookIndex') == undefined) {
+            myPub.setCookie('cookIndex','-1',cTime);
+            cookIndex = parseInt(myPub.getCookie('cookIndex'));
+        }
+        else {
+            oHisListUl.empty();
+            for(var i=0; i<cookArry.length; i++) {
+                if(cookArry[i].charAt(0) == 'r') {
+                    _val[i] = cookArry[i].substring( cookArry[i].indexOf('=')+1 );
+                    oHisListUl.prepend('<li>'+ _val[i] +'</li>');
+                }
+
+            }
+
+        }
+
+        //搜索后添加cookie
+        oSearchBtn.hammer().on('tap',function() {
+            if(oSearchTxt.val() == '') {
+                myPub.TipLayer('亲！搜索信息不能为空！');
+            }
+            else {
+                cookIndex == null || cookIndex >= 4  ? cookIndex = 0 : cookIndex++;
+                myPub.setCookie('records'+cookIndex,oSearchTxt.val(),cTime);
+                myPub.setCookie('cookIndex',cookIndex,cTime);
+            }
+        });
+
+        // 清楚cookie和历史记录
+        oHClearBtn.hammer().on('tap',function() {
+            myPub.askLayer('亲！您确定要清除历史记录？',function() {
+                for(var i=0; i<cookArry.length; i++) {
+                    myPub.delCookie('records'+i);
+                }
+                myPub.delCookie('cookIndex');
+                // 清楚cookie的记录 添加系统推荐的
+                oHisListUl.empty().html(recomHTMl);
+                myPub.LayerCloseAll();
+            },function() {
+                alert('no')
+            },'no');
+        });
+
 
 /**********************************  部分公共操作 end     **********************************/
 
